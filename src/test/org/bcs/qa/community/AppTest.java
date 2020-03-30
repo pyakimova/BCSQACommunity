743d69fd689ca9f56595523c4fafea114d288425
BerezinAA <BerezinAA@bcs.ru>
2020-03-30 15:52:40 +0700

    Пытаюсь подключить аллюр

--- src/test/org/bcs/qa/community/AppTest.java
+++ src/test/org/bcs/qa/community/AppTest.java
@@ -1,10 +1,12 @@
 package org.bcs.qa.community;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static org.junit.Assert.assertTrue;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 /**
  * Unit test for simple App.
@@ -17,5 +19,13 @@ public class AppTest
     @Test
     public void CheckGoogle(){
         open("https://google.com");
+    }
+
+    @Test
+    public void userCanSearchAnyKeyword() {
+        open("https://google.com/ncr");
+        $(By.name("q")).val("selenide").pressEnter();
+        $$("#res .g").shouldHave(sizeGreaterThan(5));
+        $("#res .g").shouldHave(text("selenide.org"));
     }
 }
