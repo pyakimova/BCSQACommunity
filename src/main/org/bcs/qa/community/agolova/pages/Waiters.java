c04874f2f7f84b1baa95d8bea804346dfb938f78
winone6666 <51526930+winone6666@users.noreply.github.com>
2020-04-03 13:20:09 +0700

    PageObject with Selenide

---
+++ src/main/org/bcs/qa/community/agolova/pages/Waiters.java
@@ -0,0 +1,22 @@
+package agolova.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import java.util.Arrays;
+import java.util.Collection;
+import com.codeborne.selenide.Condition;
+
+public class Waiters {
+
+    private Waiters() {
+
+    }
+
+    public static void waitElementsUntil(Condition selenideCondition, int timeout, SelenideElement... selenideElements) {
+        Arrays.stream(selenideElements).forEach(e -> e.waitUntil(selenideCondition, timeout));
+    }
+
+    public static void waitElementsUntil(Condition selenideCondition, int timeout, Collection<SelenideElement> selenideElements) {
+        selenideElements.forEach(e -> e.waitUntil(selenideCondition, timeout));
+    }
+}
