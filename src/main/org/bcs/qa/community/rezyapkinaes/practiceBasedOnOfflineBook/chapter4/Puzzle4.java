b6c85a97b789957f1bf30b4a37d41a5570d62533
RezyapkinaES <rezjabcheg@gmsil.com>
2020-04-07 15:04:09 +0700

    решение тестовых задач из учебника по Java

--- src/main/org/bcs/qa/community/rezyapkinaes/practiceBasedOnOfflineBook/chapter4/Puzzle4.java
+++ src/main/org/bcs/qa/community/rezyapkinaes/practiceBasedOnOfflineBook/chapter4/Puzzle4.java
@@ -1,22 +1,27 @@
 package main.org.bcs.qa.community.rezyapkinaes.practiceBasedOnOfflineBook.chapter4;
 
 public class Puzzle4 {
-    public static void main(String[] args) {
+    public static void main (String [] args) {
         Puzzle4B[] obs = new Puzzle4B[6];
         int y = 1;
         int x = 0;
         int result = 0;
-        while (x > 5) {
+        while (x < 6) {
             obs[x] = new Puzzle4B();
             obs[x].ivar = y;
+            y = y * 10;
             x = x + 1;
+        }
+        x = 6;
+        while (x > 0) {
+            x = x - 1;
             result = result + obs[x].doStuff(x);
         }
+        System.out.println("результат" + " " + result);
     }
-
-    private static class Puzzle4B {
-        public int ivar;
-
+  }
+class Puzzle4B {
+        int ivar;
         public int doStuff(int factor) {
             if (ivar > 100) {
                 return ivar * factor;
@@ -26,4 +31,3 @@ public class Puzzle4 {
             }
         }
     }
-}
